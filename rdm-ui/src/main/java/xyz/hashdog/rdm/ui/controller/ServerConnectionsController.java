fd76cca13ae0a5d47b1bbaa87b2d64cabef0658a
th <1103617438@qq.com>
2023-07-20 00:12:41 +0800

    增加一些基础命令的测试

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/ServerConnectionsController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/ServerConnectionsController.java
@@ -1,12 +1,13 @@
 package xyz.hashdog.rdm.ui.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,6 +17,18 @@ import java.io.IOException;
 public class ServerConnectionsController {
     @FXML
     public AnchorPane root;
+    /**
+     * 上边栏的按钮父节点
+     */
+    @FXML
+    public HBox buttonsHBox;
+    /**
+     * 右键菜单
+     */
+    @FXML
+    public ContextMenu contextMenu;
+    @FXML
+    public Button bottomConnectButton;
     private Stage newConnctionWindowStage;
 
     @FXML
@@ -25,7 +38,43 @@ public class ServerConnectionsController {
     @FXML
     public void initialize() {
         initTreeView();
+        initListener();
 
+    }
+
+    private void initListener() {
+        treeViewListener();
+    }
+
+    private void treeViewListener() {
+        treeView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            boolean isLeafNode = newValue != null && newValue.isLeaf();
+            // 使用选择器获取一组按钮,叶子节点才能连接,非叶子节点才能新建分组和新建连接
+            buttonsHBox.lookupAll(".isLeafNode").forEach(node -> {
+                Button button = (Button) node;
+                button.setDisable(!isLeafNode);
+            });
+            buttonsHBox.lookupAll(".isNotLeafNode").forEach(node -> {
+                Button button = (Button) node;
+                button.setDisable(isLeafNode);
+            });
+            // 右键菜单显示/隐藏
+            ObservableList<MenuItem> items = contextMenu.getItems();
+            items.forEach(menuItem -> {
+                if (menuItem.getStyleClass().contains("isLeafNode")) {
+                    //禁用/隐藏
+//                    menuItem.setDisable(!isLeafNode);
+                    menuItem.setVisible(isLeafNode);
+                }
+                if (menuItem.getStyleClass().contains("isNotLeafNode")) {
+                    menuItem.setVisible(!isLeafNode);
+//                    menuItem.setDisable(isLeafNode);
+                }
+            });
+            //连接按钮禁用否
+            bottomConnectButton.setDisable(!isLeafNode);
+
+        });
     }
 
     private void initTreeView() {
