876f0b4d3106e9f17d8287574ecf4d2e54ea1965
hod <1103617438@qq.com>
2025-07-04 21:04:40 +0800

    refactor(rdm-ui): 添加 App 类作为应用主类
    - 在包 xyz.hashdog.rdm.ui 下创建 App 类
    - 添加 main 方法，作为应用入口
    - 在 App 类中添加应用主类的文档注释

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/App.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/App.java
@@ -1,5 +1,8 @@
 package xyz.hashdog.rdm.ui;
 
+/**
+ * Main class for the application.
+ */
 public class App {
     public static void main(String[] args) {
         xyz.hashdog.rdm.ui.Main.main(args);
