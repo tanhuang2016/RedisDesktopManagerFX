c975addf4c2be42004b2a6ae7436994a25e64f0f
1103617438@qq.com <1103617438@qq.com>
2025-07-08 19:59:12 +0800

    refactor(rdm-ui): 为 SettingsController 添加 TreeView 属性并更新 FXML
    - 在 SettingsController 中添加 treeView 属性，类型为 TreeView
    - 更新 SettingsView.fxml，为 TreeView 添加 fx:id="treeView"

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/SettingsController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/SettingsController.java
@@ -1,11 +1,13 @@
 package xyz.hashdog.rdm.ui.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
 
 public class SettingsController  extends BaseWindowController<MainController>{
     @FXML
     public AnchorPane root;
+    public TreeView treeView;
 
     @FXML
     public void initialize() {
