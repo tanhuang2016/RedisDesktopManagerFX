f1419a09a9e8d69bc2ff8fa2ac5abaf6c6c038b4
1103617438@qq.com <1103617438@qq.com>
2025-07-14 09:47:56 +0800

    refactor(rdm-ui): 重新布局集合类型视图并美化按钮样式
    - 将添加和删除按钮移动到顶部栏，优化界面布局
    - 为 add 和 delRow 按钮添加样式类，提升视觉效果
    -调整 VBox 的 prefWidth 为 0，以适应新的布局结构

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/SetTypeController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/SetTypeController.java
@@ -99,6 +99,12 @@ public class SetTypeController extends BaseKeyController<KeyTabController> imple
         save.getStyleClass().add(Styles.ACCENT);
         findButton.getStyleClass().addAll(Styles.BUTTON_ICON,Styles.FLAT,Styles.ROUNDED,Styles.SMALL);
         findButton.setCursor(Cursor.HAND);
+        add.getStyleClass().addAll(
+                Styles.BUTTON_OUTLINED, Styles.ACCENT
+        );
+        delRow.getStyleClass().addAll(
+                Styles.BUTTON_OUTLINED, Styles.DANGER
+        );
     }
     private void initButtonIcon() {
         findButton.setGraphic(new FontIcon(Feather.SEARCH));
