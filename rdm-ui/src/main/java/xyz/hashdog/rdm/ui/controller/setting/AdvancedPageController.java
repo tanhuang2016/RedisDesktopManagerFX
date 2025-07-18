29eb73f0d14c8cac6db005d3f79e5561fee19a48
1103617438@qq.com <1103617438@qq.com>
2025-07-18 16:43:05 +0800

    feat(setting): 添加高级设置页面的连接信息配置
    - 在 AdvancedPage.fxml 中添加了连接信息配置的 UI 元素，包括 key separator 和展示风格设置
    - 在 AdvancedPageController 中添加了初始化方法，目前未实现具体逻辑
    -此次修改为高级设置页面增加了新的功能，使用户能够自定义连接信息的显示方式

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/AdvancedPageController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/AdvancedPageController.java
@@ -8,16 +8,12 @@ import xyz.hashdog.rdm.ui.controller.MainController;
 import xyz.hashdog.rdm.ui.sampler.layout.MainModel;
 import xyz.hashdog.rdm.ui.sampler.layout.Sidebar;
 
-public class AdvancedPageController  extends BaseWindowController<MainController> {
+public class AdvancedPageController  {
     @FXML
     public AnchorPane root;
-    public TreeView treeView;
-    public AnchorPane sidebarParent;
 
     @FXML
     public void initialize() {
-        Sidebar sidebar = new Sidebar(new MainModel());
-        sidebarParent.getChildren().add(sidebar);
 
     }
 }
