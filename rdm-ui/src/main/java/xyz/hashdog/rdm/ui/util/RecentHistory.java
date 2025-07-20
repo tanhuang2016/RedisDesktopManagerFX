9a8d3e2354063d746f1f386c0e3c9a3928b4f9fd
hod <1103617438@qq.com>
2025-07-20 11:39:34 +0800

    feat(ui): 新增最近记录保存功能
    - 实现了一个通用的最近历史记录保存类 RecentHistory- 可用于保存搜索记录、打开最近连接等功能
    - 支持自定义通知接口 Noticer- 添加了基本的增删查操作，以及最大记录数限制

---
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/util/RecentHistory.java
@@ -0,0 +1,47 @@
+package xyz.hashdog.rdm.ui.util;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+/**
+ * 最近记录保存，用于搜索记录，打开最近连接等功能
+ * @author th
+ * @version 2.0.0
+ * @since 2025/7/20 13:10
+ */
+public class RecentHistory<T> {
+
+    private int size = 5;
+    private final LinkedHashSet<T> historySet = new LinkedHashSet<>();
+    private Noticer<T> noticer;
+
+    public RecentHistory(int size, Noticer<T> noticer) {
+        this.size = size;
+        this.noticer = noticer;
+    }
+
+    public void add(T query) {
+        historySet.remove(query);
+        if (historySet.size() >= size) {
+            historySet.removeLast();
+        }
+        boolean add = historySet.add(query);
+        if(add){
+            //通知
+        }
+
+    }
+
+    public List<T> get() {
+        List<T> list = new ArrayList<>(historySet);
+        if (list.size() > size) {
+            return list.subList(0, size);
+        }
+        return list;
+    }
+    public static interface Noticer<T>{
+        void notice(List<T> list);
+
+    }
+}
