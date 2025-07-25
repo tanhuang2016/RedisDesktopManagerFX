846d6db3305e891acca4c05a4c89e9e3f94b7cb0
1103617438@qq.com <1103617438@qq.com>
2025-07-25 15:40:15 +0800

    feat(setting): 将连接超时和执行超时的输入框改为可编辑的 Spinner 控件
    - 在 AdvancedPage.fxml 中添加 Spinner 控件替代 TextField
    - 在 AdvancedPageController 中初始化 Spinner 值工厂
    - 添加整数输入过滤方法，确保用户只能输入有效数值- 优化页面初始化，加载高级设置数据

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/AdvancedPageController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/AdvancedPageController.java
@@ -4,21 +4,28 @@ import atlantafx.base.theme.Styles;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.kordamp.ikonli.material2.Material2AL;
 import org.kordamp.ikonli.material2.Material2MZ;
+import xyz.hashdog.rdm.ui.common.Applications;
+import xyz.hashdog.rdm.ui.common.ConfigSettingsEnum;
 import xyz.hashdog.rdm.ui.controller.BaseWindowController;
 import xyz.hashdog.rdm.ui.controller.MainController;
+import xyz.hashdog.rdm.ui.entity.config.AdvancedSetting;
+import xyz.hashdog.rdm.ui.entity.config.KeyTagSetting;
 import xyz.hashdog.rdm.ui.sampler.layout.MainModel;
 import xyz.hashdog.rdm.ui.sampler.layout.Sidebar;
 import xyz.hashdog.rdm.ui.util.GuiUtil;
 
+import java.util.List;
+
 public class AdvancedPageController  {
     @FXML
     public AnchorPane root;
-    public TextField connectionTimeout;
-    public TextField soTimeout;
+    public Spinner<Integer> connectionTimeout;
+    public Spinner<Integer> soTimeout;
     public ToggleButton treeShow;
     public ToggleButton listShow;
     public TextField keySeparator;
@@ -28,6 +35,29 @@ public class AdvancedPageController  {
     @FXML
     public void initialize() {
         initToggleButton();
+        initSpinner();
+        initData();
+        initButton();
+//        addListener();
+    }
+    private void initSpinner() {
+        connectionTimeout.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 60_000, 6000));
+        soTimeout.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 60_000, 6000));
+    }
+
+    private void initData() {
+        AdvancedSetting setting = Applications.getConfigSettings(ConfigSettingsEnum.ADVANCED.name);
+        connectionTimeout.getEditor().setText(String.valueOf(setting.getConnectionTimeout()));
+        soTimeout.getEditor().setText(String.valueOf(setting.getConnectionTimeout()));
+        treeShow.setSelected(setting.isTreeShow());
+        listShow.setSelected(!setting.isTreeShow());
+
+    }
+    private void initButton() {
+        initButtonStyles();
+    }
+    private void initButtonStyles() {
+        ok.getStyleClass().add(Styles.ACCENT);
     }
 
     private void initToggleButton() {
@@ -54,5 +84,16 @@ public class AdvancedPageController  {
     }
 
     public void reset(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void filterIntegerInput(KeyEvent keyEvent) {
+        // 获取用户输入的字符
+        String inputChar = keyEvent.getCharacter();
+        // 如果输入字符不是整数，则阻止其显示在TextField中
+        if (!inputChar.matches("\\d")) {
+            keyEvent.consume();
+        }
     }
 }
