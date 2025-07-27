246d264fbc4548ad258edafa92f42d229d585017
hod <1103617438@qq.com>
2025-07-27 11:43:25 +0800

    feat(ui): 添加 TabPaneEvent 类
    新增 TabPaneEvent 类用于处理主题相关事件，包括主题更改、字体更改、颜色更改以及主题的添加和移除。该类继承自 Event 类，并包含一个枚举类型 EventType 用于定义不同的事件类型。

---
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/sampler/event/TabPaneEvent.java
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: MIT */
+
+package xyz.hashdog.rdm.ui.sampler.event;
+
+public final class TabPaneEvent extends Event {
+
+    public enum EventType {
+        // theme can change both, base font size and colors
+        THEME_CHANGE,
+        // font size or family only change
+        FONT_CHANGE,
+        // colors only change
+        COLOR_CHANGE,
+        // new theme added or removed
+        THEME_ADD,
+        THEME_REMOVE
+    }
+
+    private final EventType eventType;
+
+    public TabPaneEvent(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    @Override
+    public String toString() {
+        return "ThemeEvent{"
+            + "eventType=" + eventType
+            + "} " + super.toString();
+    }
+}
