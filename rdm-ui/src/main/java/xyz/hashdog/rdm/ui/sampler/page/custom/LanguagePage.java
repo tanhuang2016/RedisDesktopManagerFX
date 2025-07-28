01edf3048b9f11dfe15eaab103d4261209a2df8b
1103617438@qq.com <1103617438@qq.com>
2025-07-28 10:18:13 +0800

    feat(ui): 添加语言设置页面- 新增 LanguagePage 类实现语言设置功能 - 添加页面头部和格式化文本说明 - 加载 LanguagePage.fxml 文件以显示语言设置界面

---
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/sampler/page/custom/LanguagePage.java
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: MIT */
+
+package xyz.hashdog.rdm.ui.sampler.page.custom;
+
+import javafx.scene.layout.AnchorPane;
+import xyz.hashdog.rdm.common.tuple.Tuple2;
+import xyz.hashdog.rdm.ui.sampler.page.AbstractPage;
+import xyz.hashdog.rdm.ui.util.GuiUtil;
+
+import java.io.IOException;
+
+public final class LanguagePage extends AbstractPage {
+    public static final String NAME = "Language";
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public LanguagePage() throws IOException {
+        super();
+
+        addPageHeader();
+        addFormattedText("""
+            A menu bar is a user interface component that typically appears at the top of \
+            an application window or screen, and provides a series of drop-down menus that \
+            allow users to access various features and functions of the application."""
+        );
+        Tuple2<AnchorPane, Object> tuple2 = GuiUtil.doLoadFXML("/fxml/setting/LanguagePage.fxml");
+        addNode(tuple2.getT1());
+    }
+
+
+
+}
