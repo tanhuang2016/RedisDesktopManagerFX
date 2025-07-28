0bef4d16f8daddb6f77cbed4db3860d0da2dbe09
1103617438@qq.com <1103617438@qq.com>
2025-07-28 11:01:33 +0800

    feat(ui): 实现语言设置页面
    - 优化 LanguageManager 类，重命名并添加新方法 getSupportedLocales
    - 在 LanguagePage.fxml 中添加语言选择组合框
    - 实现 LanguagePageController 中的语言选择逻辑
    - 添加支持语言列表和当前语言设置

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/LanguagePageController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/LanguagePageController.java
@@ -1,13 +1,60 @@
 package xyz.hashdog.rdm.ui.controller.setting;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.StringConverter;
+import xyz.hashdog.rdm.common.util.DataUtil;
+import xyz.hashdog.rdm.ui.util.LanguageManager;
+
+import java.util.List;
+import java.util.Locale;
 
 public class LanguagePageController {
     public Button ok;
     public Button system;
     public AnchorPane root;
+    public ComboBox<Locale> langComboBox;
+
+    @FXML
+    public void initialize() {
+        initLangComboBox();
+
+    }
+
+    private void initLangComboBox() {
+        // 获取支持的语言列表
+        List<Locale> supportedLocales = LanguageManager.getSupportedLocales();
+
+        // 设置ComboBox显示
+        langComboBox.setItems(FXCollections.observableArrayList(supportedLocales));
+        langComboBox.setConverter(new StringConverter<Locale>() {
+            @Override
+            public String toString(Locale locale) {
+                if (locale != null) {
+//                    if (DataUtil.isBlank(locale.toString())) {
+//                        return String.format("%s", "English");
+//                    }
+                    return String.format("%s", locale.getDisplayName(locale));
+                }
+                return "";
+            }
+
+            @Override
+            public Locale fromString(String string) {
+                return null;
+            }
+        });
+
+
+        // 设置当前语言
+        langComboBox.setValue(Locale.getDefault());
+
+    }
 
     public void ok(ActionEvent actionEvent) {
     }
--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/util/LanguageManager.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/util/LanguageManager.java
@@ -1,5 +1,7 @@
 package xyz.hashdog.rdm.ui.util;
 
+import xyz.hashdog.rdm.common.util.DataUtil;
+
 import java.util.*;
 
 public class LanguageManager {
@@ -7,14 +9,17 @@ public class LanguageManager {
     public static final String BASE_NAME = "i18n.messages";
 
     public static Locale DEFAULT_LOCALE = Locale.getDefault();
-    public static List<Locale> getAvailableLocales(String baseName) {
+    public static List<Locale> getSupportedLocales() {
         List<Locale> availableLocales = new ArrayList<>();
 
         // 获取系统支持的所有语言环境
         Locale[] locales = Locale.getAvailableLocales();
         for (Locale locale : locales) {
+            if(DataUtil.isBlank(locale.toString())){
+                continue;
+            }
             try {
-                ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale);
+                ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale);
                 if (bundle.getLocale().equals(locale) ||
                         bundle.getLocale().equals(Locale.ROOT)) {
                     availableLocales.add(locale);
