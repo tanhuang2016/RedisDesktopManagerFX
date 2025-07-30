688130bdabb1088c30d44d9858a2525b67f99a10
1103617438@qq.com <1103617438@qq.com>
2025-07-30 16:42:04 +0800

    feat(rdm-ui): 添加自动刷新功能
    - 在 RefreshPopover.fxml 中添加了自动刷新开关和刷新间隔输入框
    - 在 RefreshPopover.java 中实现了自动刷新逻辑
    - 添加了输入框的焦点事件监听，

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/popover/RefreshPopover.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/popover/RefreshPopover.java
@@ -1,4 +1,64 @@
 package xyz.hashdog.rdm.ui.controller.popover;
 
-public class RefreshPopover {
+import atlantafx.base.controls.ToggleSwitch;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.util.Duration;
+import xyz.hashdog.rdm.ui.util.GuiUtil;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public  class RefreshPopover implements Initializable {
+    public ToggleSwitch autoRefreshToggleSwitch;
+    public TextField rate;
+    private Timeline refreshTimeline;
+    private Integer rateValue;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        GuiUtil.filterIntegerInput(rate);
+        autoRefreshToggleSwitch.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                startAutoRefresh();
+            } else {
+                stopAutoRefresh();
+            }
+        });
+        // 监听光标离开输入框事件（失去焦点）
+        rate.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            // 如果 newValue 为 false，表示光标离开了输入框
+            if (!newValue) {
+                // 如果自动刷新正在运行，重启它以应用新的时间间隔
+                if (autoRefreshToggleSwitch.isSelected() && rateValue!=Integer.parseInt(rate.getText())) {
+                    stopAutoRefresh();
+                    startAutoRefresh();
+                }
+            }
+        });
+
+    }
+
+    private void stopAutoRefresh() {
+        if (refreshTimeline != null) {
+            refreshTimeline.stop();
+        }
+    }
+
+    private void startAutoRefresh() {
+        rateValue=Integer.parseInt(rate.getText());
+        // 创建时间线，每5秒执行一次
+        refreshTimeline = new Timeline(
+                new KeyFrame(Duration.seconds(rateValue), event -> refresh())
+        );
+        refreshTimeline.setCycleCount(Timeline.INDEFINITE);
+        refreshTimeline.play();
+    }
+
+    private void refresh() {
+        System.out.println("刷新");
+    }
 }
